0f4e5c449ff1625d619305460c51c620eecab6b4
Qelemnaz90 <enazy1990@gmail.com>
2024-04-01 22:21:51 +0400

    index

---
+++ index.js
@@ -0,0 +1,28 @@
+const result = document.getElementById('result');
+
+
+
+
+function appendToDisplay(input){
+    result.value += input
+}
+
+
+function clearDisplay(){
+    result.value = ""
+}
+
+function calculate (){
+    try{
+        result.value = eval(result.value)
+    }
+    catch(error){
+        result.value = "Error"
+    }
+}
+
+
+
+
+
+
